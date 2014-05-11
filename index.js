5b5d465f496859bf6354e775e922d2db33067930
Florian Reiterer <me@florianreiterer.com>
2014-05-11 15:08:33 +0200

    leave error handling to library users

--- index.js
+++ index.js
@@ -3,19 +3,14 @@ var SourceMapGenerator = require('source-map').SourceMapGenerator;
 var SourceMapConsumer = require('source-map').SourceMapConsumer;
 
 module.exports = function applySourceMap(file, sourceMap) {
-  try {
-    if (typeof sourceMap === 'string' || sourceMap instanceof String) {
-      sourceMap = JSON.parse(sourceMap);
-    }
-    if (file.sourceMap) {
-      var generator = SourceMapGenerator.fromSourceMap(new SourceMapConsumer(sourceMap));
-      generator.applySourceMap(new SourceMapConsumer(file.sourceMap));
-      file.sourceMap = JSON.parse(generator.toString());
-    } else {
-      file.sourceMap = sourceMap;
-    }
-  } catch (e) {
-    console.error('Error applying source map:');
-    console.error(e.stack);
+  if (typeof sourceMap === 'string' || sourceMap instanceof String) {
+    sourceMap = JSON.parse(sourceMap);
+  }
+  if (file.sourceMap) {
+    var generator = SourceMapGenerator.fromSourceMap(new SourceMapConsumer(sourceMap));
+    generator.applySourceMap(new SourceMapConsumer(file.sourceMap));
+    file.sourceMap = JSON.parse(generator.toString());
+  } else {
+    file.sourceMap = sourceMap;
   }
 };
